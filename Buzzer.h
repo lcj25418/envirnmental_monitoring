b0ddaa9bc40012e9e7371ec27bc647ad6400a575
LCJ25418 <1824636005@qq.com>
2025-06-18 11:29:37 +0800

    彻底清空 master 分支文件

--- Buzzer.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BUZZER_H
-#define __BUZZER_H
-
-void Buzzer_Init(void);			//蜂鸣器初始化函数
-void Buzzer_ON(void);			//蜂鸣器开启函数
-void Buzzer_OFF(void);			//蜂鸣器关闭函数
-
-#endif
--- InfrarSensor.h
+++
@@ -1,7 +0,0 @@
-#ifndef __INFRARSENSOR_H
-#define __INFRARSENSOR_H
-
-void InfrarSensor_Init(void);		//红外传感器初始化
-uint8_t Infrar_Detected(void);		//是否检测到物体函数
-
-#endif
